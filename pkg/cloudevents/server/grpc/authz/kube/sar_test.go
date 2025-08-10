a0045142ed61dd5cc9dceab06a48cb1bb093702f
xuezhaojun <zxue@redhat.com>
2025-08-10 22:55:17 +0800

    Refactor grpc pkg to support general GRPC service.
    Signed-off-by: xuezhaojun <zxue@redhat.com>

--- pkg/cloudevents/server/grpc/authz/kube/sar_test.go
+++ pkg/cloudevents/server/grpc/authz/kube/sar_test.go
@@ -17,7 +17,7 @@ import (
 	"open-cluster-management.io/sdk-go/pkg/cloudevents/clients/lease"
 	"open-cluster-management.io/sdk-go/pkg/cloudevents/clients/work/payload"
 	"open-cluster-management.io/sdk-go/pkg/cloudevents/generic/types"
-	"open-cluster-management.io/sdk-go/pkg/cloudevents/server/grpc/authn"
+	"open-cluster-management.io/sdk-go/pkg/server/grpc/authn"
 )
 
 func TestSARAuthorize(t *testing.T) {
@@ -197,7 +197,7 @@ func TestSARAuthorize(t *testing.T) {
 
 			auth := NewSARAuthorizer(client)
 
-			err := auth.Authorize(tc.userCtx(), tc.cluster, tc.eventsType)
+			err := auth.authorize(tc.userCtx(), tc.cluster, tc.eventsType)
 			if tc.expectErr && err == nil {
 				t.Errorf("expected error, got nil")
 			}
---
+++ pkg/server/grpc/authz/interface.go
@@ -0,0 +1,16 @@
+package authz
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+)
+
+type UnaryAuthorizer interface {
+	AuthorizeRequest(ctx context.Context, req any) error
+}
+
+type StreamAuthorizer interface {
+	AuthorizeStream(ctx context.Context, ss grpc.ServerStream, info *grpc.StreamServerInfo) (grpc.ServerStream, error)
+	FullMethodName() string
+}
